9e2e6bb13f51cf5ba985fae5fb594dca3c213195
Jessica <cjessica@mit.edu>
2013-09-25 15:31:07 -0400

    main activity

--- MyApp/src/com/example/myapp/MainActivity.java
+++ MyApp/src/com/example/myapp/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.myapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "LoggerActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		Log.d(TAG,"THIS IS MY LOGGING LINE---IT'S GREAT FOR DEBUGGING");
     }
 
 
